ccd1eac1a3bc294d9090f288c99a1ef8926d9f99
김강토 <kangsansam@naver.com>
2024-11-05 15:04:12 +0900

    Deleted PLANe/.mvn/wrapper/maven-wrapper.properties, PLANe/src/main/java/com/plane/common/exception/DuplicatePhoneException.java, PLANe/src/main/java/com/plane/common/exception/ErrorCode.java, PLANe/src/main/java/com/plane/common/exception/InvalidPasswordException.java, PLANe/src/main/java/com/plane/common/exception/InvalidRequestException.java, PLANe/src/main/java/com/plane/common/response/ApiResponse.java, PLANe/src/main/java/com/plane/common/util/HashUtil.java, PLANe/src/main/java/com/plane/user/controller/UserController.java, PLANe/src/main/java/com/plane/user/domain/User.java, PLANe/src/main/java/com/plane/user/dto/UserLoginRequest.java, PLANe/src/main/java/com/plane/user/dto/UserLoginResponse.java, PLANe/src/main/java/com/plane/user/dto/UserSignupRequest.java, PLANe/src/main/java/com/plane/user/mapper/UserMapper.java, PLANe/src/main/java/com/plane/user/repository/UserRepository.java, PLANe/src/main/java/com/plane/user/repository/UserRepositoryImpl.java, PLANe/src/main/java/com/plane/user/service/UserService.java, PLANe/src/main/java/com/plane/user/service/UserServiceImpl.java, PLANe/src/main/java/com/plane/PLANeApplication.java, PLANe/src/main/resources/application.properties, PLANe/src/resource/schema.sql, PLANe/src/test/java/com/plane/user/controller/UserControllerTest.java, PLANe/src/test/java/com/plane/user/service/UserServiceTest.java, PLANe/src/test/java/com/plane/PlaNeApplicationTests.java, PLANe/.gitattributes, PLANe/.gitignore, PLANe/HELP.md, PLANe/mvnw, PLANe/mvnw.cmd, PLANe/pom.xml

--- PLANe/src/main/java/com/plane/common/exception/InvalidPasswordException.java
+++
@@ -1,9 +0,0 @@
-package com.plane.common.exception;
-
-public class InvalidPasswordException extends InvalidRequestException {
-
-	public InvalidPasswordException(String message) {
-		super(ErrorCode.INVALID_PASSWORD);
-	}
-	
-}
