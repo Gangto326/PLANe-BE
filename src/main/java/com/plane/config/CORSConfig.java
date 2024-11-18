84e91a96ce79da2d460b55ec7b150fed2c4ac4f9
김강토 <kangsansam@naver.com>
2024-11-18 17:41:05 +0900

    :recycle: refactor: CORS 변경

--- src/main/java/com/plane/config/CORSConfig.java
+++ src/main/java/com/plane/config/CORSConfig.java
@@ -11,15 +11,19 @@ public class CORSConfig implements WebMvcConfigurer {
 	public void addCorsMappings(CorsRegistry registry) {
 		
 		registry
-			.addMapping("/**") // 허용하려는 API 요청 경로
+			.addMapping("/api/**") // 허용하려는 API 요청 경로
 			.allowedMethods("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS") // 허용하려는 HTTP Method 설정 (OPTIONS는 Preflight 설정)
-			.allowedOrigins("http://localhost:5173/") // 허용하려는 클라이언트 측 주소
+			.allowedOrigins(
+//					"http://localhost:5173",
+					"*"
+					) // 허용하려는 클라이언트 측 주소
+			.allowedHeaders("*")
 			.allowCredentials(true) // HttpOnly Cookie를 사용하기 위한 설정
 			.exposedHeaders("Access-Control-Allow-Headers")
 			.exposedHeaders("Authorization") // 클라이언트에서 해당 Header의 값을 확인할 수 있도록 설정.
 			.maxAge(3600); // Preflight Cache 설정
 		
-		WebMvcConfigurer.super.addCorsMappings(registry);
+//		WebMvcConfigurer.super.addCorsMappings(registry);
 	}
 	
 }
